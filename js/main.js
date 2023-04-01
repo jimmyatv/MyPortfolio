f3966d5c560fcdb6748a507e1e0eea28305f58bc
jimmyatv <rasic.alexandar@gmail.com>
2023-04-01 10:50:30 +0200

    jQuery practice, About me in header added

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// $('h1').hide(3000); // test jQuery !
+
+$(document).ready(function() {
+
+    //? FADEOUT EFFECT
+    //   let typed = new Typed('.element', {
+    //     strings: ['make a website for you.', 'create a web application for you.', 'optimize your website.'],
+    //     typeSpeed: 70,
+    //     backSpeed: 50,
+    //     fadeOut: true,
+    //     loop: true,
+    //   });
+
+
+      //? SMART BACKSPACE
+      var typed = new Typed('.element', {
+        strings: ['make a website for you.', 'create a web application for you.', 'optimize your website.'],
+        typeSpeed: 60,
+        backSpeed: 10,
+        smartBackspace: true, // Default
+        loop: true
+      });
+
+});
